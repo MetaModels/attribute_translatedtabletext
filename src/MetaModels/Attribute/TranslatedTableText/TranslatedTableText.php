11cc42b0614889d66fc290a5a186e528fb8dff12
Sven Baumann <baumann.sv@gmail.com>
2016-12-08 16:52:31 +0100

    update header php doc

--- src/MetaModels/Attribute/TranslatedTableText/TranslatedTableText.php
+++ src/MetaModels/Attribute/TranslatedTableText/TranslatedTableText.php
@@ -1,6 +1,13 @@
 <?php
 
 /**
+ * This file is part of MetaModels/attribute_translatedselect.
+ *
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
  * The MetaModels extension allows the creation of multiple collections of custom items,
  * each with its own unique set of selectable attributes, with attribute extendability.
  * The Front-End modules allow you to build powerful listing and filtering of the
@@ -13,6 +20,7 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Andreas Isaak <andy.jared@googlemail.com>
  * @author     David Greminger <david.greminger@1up.io>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2016 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedtabletext/blob/master/LICENSE LGPL-3.0
  * @filesource
--- src/MetaModels/DcGeneral/Events/Table/Attribute/TranslatedTableText/Subscriber.php
+++ src/MetaModels/DcGeneral/Events/Table/Attribute/TranslatedTableText/Subscriber.php
@@ -1,6 +1,13 @@
 <?php
 
 /**
+ * This file is part of MetaModels/attribute_translatedselect.
+ *
+ * (c) 2012-2016 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
  * The MetaModels extension allows the creation of multiple collections of custom items,
  * each with its own unique set of selectable attributes, with attribute extendability.
  * The Front-End modules allow you to build powerful listing and filtering of the
@@ -12,6 +19,7 @@
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Andreas Nölke <zero@brothers-project.de>
  * @author     David Greminger <david.greminger@1up.io>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
  * @copyright  2012-2016 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_translatedtabletext/blob/master/LICENSE LGPL-3.0
  * @filesource
